467dda48a86264921ac9afc514bca2d240bf8749
Mohamed Adnen Taleb <adnane102464@gmail.com>
2026-06-28 19:16:18 +0100

    feat: implement basic route handling

--- server.js
+++ server.js
@@ -1,9 +1,31 @@
 import http from "http";
 
 const server = http.createServer((req, res) => {
-  console.log(`${req.method} ${req.url}`);
+  if (req.url === "/" && req.method === "GET") {
+    res.writeHead(200, {
+      "content-type": "text/plain",
+    });
 
-  res.end("Server is running!");
+    res.end("Home Page");
+  } else if (req.url === "/about" && req.method === "GET") {
+    res.writeHead(200, {
+      "content-type": "text/plain",
+    });
+
+    res.end("About Page");
+  } else if (req.url === "/contact-me" && req.method === "GET") {
+    res.writeHead(200, {
+      "content-type": "text/plain",
+    });
+
+    res.end("Contact Me");
+  } else {
+    res.writeHead(404, {
+      "content-type": "text/plain",
+    });
+
+    res.end("404 - Page Not Found");
+  }
 });
 
 server.listen(3000, () => console.log("Server is running!"));
